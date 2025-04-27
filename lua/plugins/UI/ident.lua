837bc4da0d54a354d47c044062bda125a91cd117
Alfr3d <alfr3d@Shelter.localdomain>
2025-04-26 18:11:49 -0600

    fix ident-black2

--- lua/plugins/UI/ident.lua
+++ lua/plugins/UI/ident.lua
@@ -1,38 +1,38 @@
 return function()
-require("ibl").setup(
-    {
-        indent = {
-        char = "│", -- Puedes usar "▏", "┆", etc.},
-      scope = {
-        enabled = true,
-        show_start = false,
-        show_end = false,
-        highlight = { "Function", "Label", "Keyword", "Constant" },
-        include = {
-          node_type = {
-             ["*"] = {                 -- wildcard para todos los lenguajes
-             "function",
-             "table_constructor",
-             "block",
-             "call_expression",      -- para llamadas como require()
-             "if_statement",    },
+  require("ibl").setup({
+    indent = {
+      char = "│",  -- Puedes usar "▏", "┆", etc.
+    },
+    scope = {
+      enabled    = true,
+      show_start = false,
+      show_end   = false,
+      highlight  = { "Function", "Label", "Keyword", "Constant" },
+      include    = {
+        node_type = {
+          ["*"] = {  -- wildcard para todos los lenguajes
+            "function",
+            "table_constructor",
+            "block",
+            "call_expression",  -- para llamadas como require()
+            "if_statement",
           },
         },
       },
-      whitespace = {
-        remove_blankline_trail = true,
+    },
+    whitespace = {
+      remove_blankline_trail = true,
+    },
+    exclude = {
+      filetypes = {
+        "help",
+        "lazy",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "mason",
       },
-      exclude = {
-        filetypes = {
-          "help",
-          "lazy",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "mason",
-        },
-      },
-    }
-)
+    },
+  })
 end
